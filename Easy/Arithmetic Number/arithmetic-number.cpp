3711d48896c2be300aac4334c415f51e473df0e7
Sourasish Mondal <sourasishmondal95@gmail.com>
2023-02-13 19:41:58 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Arithmetic Number/arithmetic-number.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+     int inSequence(int A, int B, int C){
+
+        // code here
+
+        if(A==B)
+
+        {
+
+            return 1;
+
+        }
+
+        if(A>0&&B<0&&C>0)
+
+        {
+
+            return 0;
+
+        }
+
+        if(C==0)
+
+        {
+
+            return A==B;
+
+        }
+
+   long long ans=(B-A);
+
+   if(ans%C==0&&ans/C>0)
+
+   {
+
+       return 1;
+
+   }
+
+       return 0;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int A, B, C;
+        cin>>A>>B>>C;
+        
+        Solution ob;
+        cout<<ob.inSequence(A, B, C)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
