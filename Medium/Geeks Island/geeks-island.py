57f7457751f53a914784fbcb4ab57c9b59387394
Sourasish Mondal <sourasishmondal95@gmail.com>
2023-02-16 00:04:24 +0530

    Updated Solution - GfG to GitHub

--- Medium/Geeks Island/geeks-island.py
+++ Medium/Geeks Island/geeks-island.py
@@ -17,7 +17,7 @@ class Solution():
     def water_flow(self, mat, n, m):
         visited1 = self.bfs(mat, n, m, [(i,0) for i in range(n)] + [(0,j) for j in range(m)])
         visited2 = self.bfs(mat, n, m, [(i,m-1) for i in range(n)] + [(n-1,j) for j in range(m)])
-        return len(visited1 & visited2)      
+        return len(visited1 & visited2) 
 
 
 #{ 
